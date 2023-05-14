b703a1e4873ccfe491ae63de0b389e43d5f93c2a
Amit Poonia <107632686+poonia007@users.noreply.github.com>
2023-05-14 20:49:19 +0530

    Add files via upload

---
+++ Project8_Login_Wave/script.js
@@ -0,0 +1,7 @@
+let labels=document.querySelectorAll('.form-control label')
+labels.forEach(label=>{
+    label.innerHTML= label.innerText
+    .split('')
+    .map((letter,index)=> `<span style=" transition-delay:${index*50}ms">${letter}</span>`)
+    .join('')
+})
